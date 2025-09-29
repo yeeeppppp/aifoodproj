42b90ac5adb9014523023f96af70db487ea871ea
ivwdie <ivwdie@icloud.com>
2025-09-29 23:14:19 +0300

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Carousel from './components/Carousel/Carousel';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path='/' element={<Carousel/>}/>
+          <Route path='/' element={<Navigation/>}/>
         </Routes>
       </div>
     </Router>
