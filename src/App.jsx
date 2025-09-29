613bbf7a5c81e4dac60557dee00e6c8804be4c5b
yeeeppppp <78420838+yeeeppppp@users.noreply.github.com>
2025-09-29 21:15:58 +0300

    втф

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 // App.jsx
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Carousel from './components/Carousel/Carousel';
 
 function App() {
   return (
     <>
-    <div></div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path='/Carousel' element={<Carousel/>}/>
+        </Routes>
+      </div>
+    </Router>
     </>
   );
 }
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,16 @@
+import "./Carousel.css";
+
+function Carousel() {
+    return(
+        <>
+            <div className="milk-stuff">
+                <h1>Молоко, яйца</h1>
+                <div className="product-container">
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Carousel;
