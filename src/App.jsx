f06e036ee288e09eb7310ea9f368289bd5aae52c
yeeeppppp <78420838+yeeeppppp@users.noreply.github.com>
2025-09-29 21:02:40 +0300

    main structure fix2
    123

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <h1>ПЕНИС</h1>
+    </>
+  );
+}
+
+export default App;
